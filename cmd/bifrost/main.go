4e2c91c62fa8f85781f845ffa582a6ff7d784bde
Xabier Larrakoetxea <me@slok.dev>
2020-04-10 12:03:09 +0200

    Add main structure
    Signed-off-by: Xabier Larrakoetxea <me@slok.dev>

---
+++ cmd/bifrost/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/slok/bifrost/internal/log"
+)
+
+// Run runs the main application.
+func Run() error {
+	logrusLog := logrus.New()
+	logger := log.NewLogrus(logrus.NewEntry(logrusLog))
+
+	logger.Infof("Hello world")
+
+	return nil
+}
+
+func main() {
+	err := Run()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error running application: %s", err)
+		os.Exit(1)
+	}
+
+	os.Exit(0)
+}
